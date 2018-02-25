a6e1f14f6e1cdf24870ce3fa7a2b08b5239d4ac7
sxoinas12 <kwnstantinosxoinas@hotmail.gr>
2018-02-25 13:11:38 +0200

    author changes

--- middlewares/author.js
+++ middlewares/author.js
@@ -6,11 +6,11 @@ var parseToken = function(req,res,next){
 		req.user = {};
 		return next();
 	}
+
 	else{
 		knex.table('Users').where('token',token).select('*').
 		then((data)=>{
-			req.user = data[0];
-			//res.send(data);
+			req.user = data[0] || {};
 			next();
 		}).catch((error)=>{
 			res.status(400).send({error:true, message:"something went wrong.."});
