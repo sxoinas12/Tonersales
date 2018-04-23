24c50a2ac573d4a7686cf1793c5d396aa57be330
Nikatlas <nikatlas@gmail.com>
2018-04-23 15:57:14 +0300

    Empty key file

---
+++ services/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+	google :{
+		clientID:'769313097300-vu1399jdu0d5qn9gj1tcc093b3dc0f42.apps.googleusercontent.com',
+		clientSecret:''
+	},
+	facebook :{
+		clientID:'174662419755044',
+		clientSecret:''
+	}
+}
